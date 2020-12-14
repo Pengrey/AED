d9e0c2622df1f82c67941e970ccf4c7c2330a58f
Pedro-Lopes-Frisson <pdfl@ua.pt>
2020-12-14 15:31:37 +0000

    process file tem de se meter nos folders certos cause lazy

--- ultimatefile_processer.sh
+++ ultimatefile_processer.sh
@@ -18,7 +18,7 @@
 ##################
 rm files/*
 dir=$( printf "%s/%s" "$(pwd)" "$1")
-files=( $(grep "End" $dir*.txt  | awk -F "/|:" '{print $9}') )
+files=( $(grep "End" 097827/*.txt  | awk -F "/|:" '{print $2}') )
 echo ${#files[@]}
 for f in ${files[@]};do
   number_of_tasks=$(awk -F "_" '{printf $1 "\n"}' <<< $f | sed "s/^0//g")
@@ -26,7 +26,8 @@ for f in ${files[@]};do
   profit_is_considered=$(awk -F "_" '{printf $3}' <<< $f )
   profit_filename=$(printf "files/profits_%02d_%02d_%s" $number_of_tasks $number_of_programmers "$profit_is_considered")
   task_filename=$(printf "files/tasks_%02d_%02d_%s" $number_of_tasks $number_of_programmers "$profit_is_considered")
-  #general_filename=$(printf "files/general_%d_%d_%s" $number_of_tasks $number_of_programmers "$profit_is_considered")
+  profits_filename=$(printf "files/profitfilename%s" "$profit_is_considered")
+  execution_filename=$(printf "files/execution%s" "$profit_is_considered")
   count=0
   for p in $(seq -1 $(( $number_of_programmers - 1)) );do
     [[ $p != -1 ]] && printf "P$p " >> $task_filename
@@ -34,7 +35,11 @@ for f in ${files[@]};do
     [[ $p != -1 ]] && printf "\n" >> $task_filename
   done;
   count=$(grep "P-1" $task_filename | wc -l)
-  head -$count $task_filename | sed "s/^P-1 //g" >> $task_filename
+  head -$count $task_filename | sed "s/^P-1 //g;s/\\t/ /g;s/  / /g" >> $task_filename
   sed -i 1,"$count"d $task_filename
-  grep "^  " 097827/$f | awk '{ printf $3"\n"}'  > $profit_filename
+  grep "^[[:digit:][:digit:]]" 097827/$f | sort -nk 1 | awk '{ printf $4"\n"}'  > $profit_filename
+  profit=$( printf "%d %d %d" $number_of_tasks $number_of_programmers $(grep "Biggest" 097827/$f | awk '{printf $4"\n"}'))
+  echo $profit >> $profits_filename
+  time_exec=$( printf "%d %d %s" $number_of_tasks $number_of_programmers "$(grep "Solution" 097827/$f | awk '{printf $4"\n"}')")
+  echo $time_exec >> $execution_filename
 done
