c2ea70bd00e95eecf81797fce2c1e1ba892d9b2a
Pedro-Lopes-Frisson <pdfl@ua.pt>
2020-12-23 14:14:16 +0000

    ULTIMATE FILE PROCESSER senao funcionar digam

--- ultimatefile_processer.sh
+++ ultimatefile_processer.sh
@@ -7,27 +7,25 @@
 #
 #   $1 for /path/to/files
 #   other argumentes will be ignored
-# tb quero uma lista com os profits em k é uma por linha e por ordem :/
-# o meu portugues tb n é muito bom
-# em caso de duvida dizeu preciso dos P's :/
-##pk tb quero as tasks k n têm P's em baixo
-#uma por linha
-#como está no file
-#sei k estou a ser chato mas foi a melhor maneira k arranjei
-#de por a funcionar :/
 ##################
-rm files/*
+
+
 dir=$( printf "%s/%s" "$(pwd)" "$1")
-files=( $(grep "End" 097827/*.txt  | awk -F "/|:" '{print $2}') )
+if [[ ! -d $dir ]] ;then  echo "O diretorio nao existe use apenas o nome da pasta ou o nome de um diretorio verdadeiro "; exit -1 ;fi
+files=( $(grep "End" $dir/*.txt | awk -F "/|:" '{print $9}') )
+
 echo ${#files[@]}
 for f in ${files[@]};do
   number_of_tasks=$(awk -F "_" '{printf $1 "\n"}' <<< $f | sed "s/^0//g")
   number_of_programmers=$(awk -F "_" '{printf $2 "\n"}' <<< $f | sed "s/^0//g")
   profit_is_considered=$(awk -F "_" '{printf $3}' <<< $f )
+
   profit_filename=$(printf "files/profits_%02d_%02d_%s" $number_of_tasks $number_of_programmers "$profit_is_considered")
+
   task_filename=$(printf "files/tasks_%02d_%02d_%s" $number_of_tasks $number_of_programmers "$profit_is_considered")
-  profits_filename=$(printf "files/profitfilename%s" "$profit_is_considered")
-  execution_filename=$(printf "files/execution%s" "$profit_is_considered")
+
+  execution_filename=$(printf "files/Execution_Times%s" "$profit_is_considered")
+
   count=0
   for p in $(seq -1 $(( $number_of_programmers - 1)) );do
     [[ $p != -1 ]] && printf "P$p " >> $task_filename
