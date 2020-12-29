9a1cef771ce49ea44d6341e50b875a736225e204
Inryatt <inrymail@gmail.com>
2020-12-29 16:27:03 +0000

    fixed ignorerofits

--- A01/job_selection.c
+++ A01/job_selection.c
@@ -71,8 +71,7 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include "../P02/elapsed_time.h"
-
+#include "../../P02/elapsed_time.h"
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
@@ -84,16 +83,15 @@
 // some C preprocessor tricks
 //
 
-#define main  rng_main                        // main gets replaced by rng_main
+#define main rng_main // main gets replaced by rng_main
 #ifdef __GNUC__
-int rng_main() __attribute__((__unused__));   // gcc will not complain if rnd_main() is not used
+int rng_main() __attribute__((__unused__)); // gcc will not complain if rnd_main() is not used
 #endif
 #include "rng.c"
-#undef main                                   // main becomes main again
+#undef main // main becomes main again
 
-#define srandom(seed)  ran_start((long)seed)  // start the pseudo-random number generator
-#define random()       ran_arr_next()         // get the next pseudo-random number (0 to 2^30-1)
-
+#define srandom(seed) ran_start((long)seed) // start the pseudo-random number generator
+#define random() ran_arr_next()             // get the next pseudo-random number (0 to 2^30-1)
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
@@ -107,18 +105,17 @@ int rng_main() __attribute__((__unused__));   // gcc will not complain if rnd_ma
 
 #if 1
 
-#define MAX_T  64  // maximum number of programming tasks
-#define MAX_P  10  // maximum number of programmers
+#define MAX_T 64 // maximum number of programming tasks
+#define MAX_P 10 // maximum number of programmers
 
 typedef struct
 {
-  int starting_date;      // I starting date of this task
-  int ending_date;        // I ending date of this task
-  int profit;             // I the profit if this task is performed
-  int assigned_to;        // S current programmer number this task is assigned to (use -1 for no assignment)
-  int best_assigned_to;        // S current programmer number this task is assigned to (use -1 for no assignment)
-}
-task_t;
+  int starting_date;    // I starting date of this task
+  int ending_date;      // I ending date of this task
+  int profit;           // I the profit if this task is performed
+  int assigned_to;      // S current programmer number this task is assigned to (use -1 for no assignment)
+  int best_assigned_to; // S current programmer number this task is assigned to (use -1 for no assignment)
+} task_t;
 
 typedef struct
 {
@@ -137,58 +134,61 @@ typedef struct
 } problem_t;
 int compare_tasks_E(const void *t1, const void *t2)
 {
-  int d1,d2;
+  int d1, d2;
 
-  d1 = ((task_t *)t1)->best_assigned_to;
-  d2 = ((task_t *)t2)->best_assigned_to;
-  if(d1 >= 0)
-    return -1;
-  else if(d2 >= 0)
-    return -1;
-  return 1;
-}
-int compare_tasks(const void *t1,const void *t2)
-{
-  int d1,d2;
-
-  d1 = ((task_t *)t1)->starting_date;
-  d2 = ((task_t *)t2)->starting_date;
-  if(d1 != d2)
-    return (d1 < d2) ? -1 : +1;
   d1 = ((task_t *)t1)->ending_date;
   d2 = ((task_t *)t2)->ending_date;
-  if(d1 != d2)
+  if (d1 != d2)
+    return (d1 < d2) ? -1 : +1;
+  d2 = ((task_t *)t2)->starting_date;
+  d1 = ((task_t *)t1)->starting_date;
+  if (d1 != d2)
     return (d1 < d2) ? -1 : +1;
   return 0;
 }
 
-void init_problem(int NMec,int T,int P,int ignore_profit,problem_t *problem)
+int compare_tasks(const void *t1, const void *t2)
 {
-  int i,r,scale,span,total_span;
+  int d1, d2;
+
+  d1 = ((task_t *)t1)->starting_date;
+  d2 = ((task_t *)t2)->starting_date;
+  if (d1 != d2)
+    return (d1 < d2) ? -1 : +1;
+  d1 = ((task_t *)t1)->ending_date;
+  d2 = ((task_t *)t2)->ending_date;
+  if (d1 != d2)
+    return (d1 < d2) ? -1 : +1;
+  return 0;
+}
+
+void init_problem(int NMec, int T, int P, int ignore_profit, problem_t *problem)
+{
+  int i, r, scale, span, total_span;
   int *weight;
   //
   // input validation
   //
-  if(NMec < 1 || NMec > 999999)
+  if (NMec < 1 || NMec > 999999)
   {
-    fprintf(stderr,"Bad NMec (1 <= NMex (%d) <= 999999)\n",NMec);
+    fprintf(stderr, "Bad NMec (1 <= NMex (%d) <= 999999)\n", NMec);
     exit(1);
   }
-  if(T < 1 || T > MAX_T)
+  if (T < 1 || T > MAX_T)
   {
-    fprintf(stderr,"Bad T (1 <= T (%d) <= %d)\n",T,MAX_T);
+    fprintf(stderr, "Bad T (1 <= T (%d) <= %d)\n", T, MAX_T);
     exit(1);
   }
-  if(P < 1 || P > MAX_P)
+  if (P < 1 || P > MAX_P)
   {
-    fprintf(stderr,"Bad P (1 <= P (%d) <= %d)\n",P,MAX_P);
+    fprintf(stderr, "Bad P (1 <= P (%d) <= %d)\n", P, MAX_P);
     exit(1);
   }
   //
   // the starting and ending dates of each task satisfy 0 <= starting_date <= ending_date <= total_span
   //
   total_span = (10 * T + P - 1) / P;
-  if(total_span < 30)
+  if (total_span < 30)
     total_span = 30;
   //
   // probability of each possible task duration
@@ -199,24 +199,24 @@ void init_problem(int NMec,int T,int P,int ignore_profit,problem_t *problem)
   // |  0  0  2  3  4  5  6  7  8  9 | 10 | 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 | 30 31 ... span
   //
   weight = (int *)alloca((size_t)(total_span + 1) * sizeof(int)); // allocate memory (freed automatically)
-  if(weight == NULL)
+  if (weight == NULL)
   {
-    fprintf(stderr,"Strange! Unable to allocate memory\n");
+    fprintf(stderr, "Strange! Unable to allocate memory\n");
     exit(1);
   }
-#define sum1  (298.0)                      // sum of weight[i] for i=2,...,29 using the data given in the comment above
-#define sum2  ((double)(total_span - 29))  // sum of weight[i] for i=30,...,data_span using a weight of 1
-#define tail  100
+#define sum1 (298.0)                     // sum of weight[i] for i=2,...,29 using the data given in the comment above
+#define sum2 ((double)(total_span - 29)) // sum of weight[i] for i=30,...,data_span using a weight of 1
+#define tail 100
   scale = (int)ceil((double)tail * 10.0 * sum2 / sum1); // we want that scale*sum1 >= 10*tail*sum2, so that large task
-  if(scale < tail)                                      // durations occur 10% of the time
+  if (scale < tail)                                     // durations occur 10% of the time
     scale = tail;
   weight[0] = 0;
   weight[1] = 0;
-  for(i = 2;i <= 10;i++)
+  for (i = 2; i <= 10; i++)
     weight[i] = scale * (2 * i);
-  for(i = 11;i <= 29;i++)
+  for (i = 11; i <= 29; i++)
     weight[i] = scale * (30 - i);
-  for(i = 30;i <= total_span;i++)
+  for (i = 30; i <= total_span; i++)
     weight[i] = tail;
 #undef sum1
 #undef sum2
@@ -224,7 +224,7 @@ void init_problem(int NMec,int T,int P,int ignore_profit,problem_t *problem)
   //
   // accumulate the weigths (cummulative distribution)
   //
-  for(i = 1;i <= total_span;i++)
+  for (i = 1; i <= total_span; i++)
     weight[i] += weight[i - 1];
   //
   // generate the random tasks
@@ -234,14 +234,14 @@ void init_problem(int NMec,int T,int P,int ignore_profit,problem_t *problem)
   problem->T = T;
   problem->P = P;
   problem->I = (ignore_profit == 0) ? 0 : 1;
-  for(i = 0;i < T;i++)
+  for (i = 0; i < T; i++)
   {
     //
     // task starting an ending dates
     //
     r = 1 + (int)random() % weight[total_span]; // 1 .. weight[total_span]
-    for(span = 0;span < total_span;span++)
-      if(r <= weight[span])
+    for (span = 0; span < total_span; span++)
+      if (r <= weight[span])
         break;
     problem->task[i].starting_date = (int)random() % (total_span - span + 1);
     problem->task[i].ending_date = problem->task[i].starting_date + span - 1;
@@ -259,7 +259,7 @@ void init_problem(int NMec,int T,int P,int ignore_profit,problem_t *problem)
     // 50 100 150 200 250 300
     //
     scale = (int)random() % 12501; // almost uniformly distributed in 0..12500
-    if(scale <= 2500)
+    if (scale <= 2500)
       problem->task[i].profit = 1 + round((double)span * (50.0 + sqrt((double)scale)));
     else
       problem->task[i].profit = 1 + round((double)span * (300.0 - 2.0 * sqrt((double)(12500 - scale))));
@@ -267,24 +267,24 @@ void init_problem(int NMec,int T,int P,int ignore_profit,problem_t *problem)
   //
   // sort the tasks by the starting date
   //
-  qsort((void *)&problem->task[0],(size_t)problem->T,sizeof(problem->task[0]),compare_tasks);
+  qsort((void *)&problem->task[0], (size_t)problem->T, sizeof(problem->task[0]), compare_tasks);
   //
   // finish
   //
-  if(problem->I != 0)
-    for(i = 0;i < problem->T;i++)
+  if (problem->I != 0)
+    for (i = 0; i < problem->T; i++)
       problem->task[i].profit = 1;
-#define DIR_NAME  problem->dir_name
-  if(snprintf(DIR_NAME,sizeof(DIR_NAME),"%06d",NMec) >= sizeof(DIR_NAME))
+#define DIR_NAME problem->dir_name
+  if (snprintf(DIR_NAME, sizeof(DIR_NAME), "%06d", NMec) >= sizeof(DIR_NAME))
   {
-    fprintf(stderr,"Directory name too large!\n");
+    fprintf(stderr, "Directory name too large!\n");
     exit(1);
   }
 #undef DIR_NAME
-#define FILE_NAME  problem->file_name
-  if(snprintf(FILE_NAME,sizeof(FILE_NAME),"%06d/%02d_%02d_%d.txt",NMec,T,P,problem->I) >= sizeof(FILE_NAME))
+#define FILE_NAME problem->file_name
+  if (snprintf(FILE_NAME, sizeof(FILE_NAME), "%06d/%02d_%02d_%d.txt", NMec, T, P, problem->I) >= sizeof(FILE_NAME))
   {
-    fprintf(stderr,"File name too large!\n");
+    fprintf(stderr, "File name too large!\n");
     exit(1);
   }
 #undef FILE_NAME
@@ -292,25 +292,24 @@ void init_problem(int NMec,int T,int P,int ignore_profit,problem_t *problem)
 
 #endif
 
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
 // problem solution (place your solution here)
 //
 
-int recurse(problem_t *prob,int t)
+int recurse(problem_t *prob, int t)
 {
 
   int busy_save;
   int profit_save;
   int task_save;
-  if(t >= prob->T)
+  if (t >= prob->T)
   {
     prob->casos++;
-    if(prob->total_profit > prob->biggest_profit)
+    if (prob->total_profit > prob->biggest_profit)
     {
       prob->biggest_profit = prob->total_profit;
-      for(int j = 0; j < prob->T; j++)
+      for (int j = 0; j < prob->T; j++)
       {
         prob->task[j].best_assigned_to = prob->task[j].assigned_to;
       }
@@ -320,35 +319,31 @@ int recurse(problem_t *prob,int t)
 
   recurse(prob, t + 1);
 
-  for(int p = 0; p < prob->P; p++)
+  for (int p = 0; p < prob->P; p++)
   {
-    if(prob->busy[p] < prob->task[t].starting_date)
+    if (prob->busy[p] < prob->task[t].starting_date && prob->task[t].assigned_to < 0)
     {
-      if(prob->task[t].assigned_to < 0)
-      {
-        busy_save = prob->busy[p];
-        task_save = prob->task[t].assigned_to;
-        profit_save = prob->total_profit;
+      busy_save = prob->busy[p];
+      task_save = prob->task[t].assigned_to;
+      profit_save = prob->total_profit;
 
-        prob->busy[p]= prob->task[t].ending_date;
-        prob->task[t].assigned_to = p ;
-        prob->total_profit += prob->task[t].profit;
-        recurse(prob, t+1);
-        prob->busy[p]= busy_save ;
-        prob->task[t].assigned_to = task_save ;
-        prob->total_profit = profit_save;
-        break;
-      }
+      prob->busy[p] = prob->task[t].ending_date;
+      prob->task[t].assigned_to = p;
+      prob->total_profit += prob->task[t].profit;
+      recurse(prob, t + 1);
+      prob->busy[p] = busy_save;
+      prob->task[t].assigned_to = task_save;
+      prob->total_profit = profit_save;
+      break;
     }
   }
   return 0;
 }
 
-
 #if 0
 int nonRec(problem_t *problem){
-#define TASK  problem->task[t]
-#define PROG  problem->busy[0]
+#define TASK problem->task[t]
+#define PROG problem->busy[0]
   int t = 0;
   PROG = TASK.ending_date;
   problem->biggest_profit+=TASK.profit;
@@ -366,6 +361,8 @@ int nonRec(problem_t *problem){
 #undef PROG
   return 1;
 }
+#endif
+#if 1
 int nonRec(problem_t *problem, int programmer){
 #define TASK  problem->task[t]
 #define PROG  problem->busy[programmer]
@@ -385,47 +382,6 @@ int nonRec(problem_t *problem, int programmer){
   return 1;
 }
 #endif
-int recurse1(problem_t *prob,int t,int p)
-{
-
-  int busy_save;
-  int profit_save;
-  int task_save;
-  if(t >= prob->T)
-  {
-    prob->casos++;
-    if(prob->total_profit > prob->biggest_profit)
-    {
-      prob->biggest_profit = prob->total_profit;
-      for(int j = 0; j < prob->T; j++)
-      {
-        prob->task[j].best_assigned_to = prob->task[j].assigned_to;
-      }
-    }
-    return 1;
-  }
-
-  recurse1(prob,p, t + 1);
-    if(prob->busy[p] < prob->task[t].starting_date)
-    {
-      if(prob->task[t].assigned_to < 0)
-      {
-        busy_save = prob->busy[p];
-        task_save = prob->task[t].assigned_to;
-        profit_save = prob->total_profit;
-
-        prob->busy[p]= prob->task[t].ending_date;
-        prob->task[t].assigned_to = p ;
-        prob->total_profit += prob->task[t].profit;
-        recurse1(prob,p, t+1);
-        prob->busy[p]= busy_save ;
-        prob->task[t].assigned_to = task_save ;
-        prob->total_profit = profit_save;
-
-    }
-  }
-  return 0;
-}
 
 #if 1
 
@@ -437,26 +393,26 @@ static void solve(problem_t *problem)
   //
   // open log file
   //
-  (void)mkdir(problem->dir_name,S_IRUSR | S_IWUSR | S_IXUSR);
-  fp = fopen(problem->file_name,"w");
-  if(fp == NULL)
+  (void)mkdir(problem->dir_name, S_IRUSR | S_IWUSR | S_IXUSR);
+  fp = fopen(problem->file_name, "w");
+  if (fp == NULL)
   {
-    fprintf(stderr,"Unable to create file %s (maybe it already exists? If so, delete it!)\n",problem->file_name);
+    fprintf(stderr, "Unable to create file %s (maybe it already exists? If so, delete it!)\n", problem->file_name);
     exit(1);
   }
   //
   // solve
   //
   problem->cpu_time = cpu_time();
-#define TASK  problem->task[t]
-  for(int t = 0; t < problem->T ; t++)
+#define TASK problem->task[t]
+  for (int t = 0; t < problem->T; t++)
   {
     TASK.assigned_to = -1;
-    TASK.best_assigned_to= -1;
+    TASK.best_assigned_to = -1;
   }
 #undef TASK
-#define PROG  problem->busy[p]
-  for(int p = 0; p < problem->P ; p++)
+#define PROG problem->busy[p]
+  for (int p = 0; p < problem->P; p++)
   {
     PROG = -1;
   }
@@ -465,29 +421,27 @@ static void solve(problem_t *problem)
   problem->biggest_profit = 0;
   problem->total_profit = 0;
 
-  qsort((void *)&problem->task[0],(size_t)problem->T,sizeof(problem->task[0]),compare_tasks);
-  if ( problem->I == 0 )
+//
+  if (problem->I == 1 && problem->P == 1)
+  {
+    qsort((void *)&problem->task[0], (size_t)problem->T, sizeof(problem->task[0]), compare_tasks_E);
+    puts("nrecurse");
+    nonRec(problem, 0);
+  }
+  else
   {
     puts("recurse");
     recurse(problem, 0);
   }
-  else
-  {
-    puts("nrecurse");
-    for(int p = 0; p < problem->P; p++)
-    {
-      recurse1(problem,0, p);
-    }
-  }
-   //  call your (recursive?) function to solve the problem here
+  // call your (recursive?) function to solve the problem here
   problem->cpu_time = cpu_time() - problem->cpu_time;
   //
   // save solution data
   //
-#define TASK  problem->task[t]
-  for(int t = 0; t < problem->T; t++)
+#define TASK problem->task[t]
+  for (int t = 0; t < problem->T; t++)
   {
-    fprintf(fp,"P%d\t%d T%d %d \n",TASK.best_assigned_to,TASK.starting_date,t,TASK.ending_date);
+    fprintf(fp, "P%d\t%d T%d %d \n", TASK.best_assigned_to, TASK.starting_date, t, TASK.ending_date);
   }
 #undef TASK
   fprintf(fp, "NMec = %d\n", problem->NMec);
@@ -502,36 +456,34 @@ static void solve(problem_t *problem)
   for (i = 0; i < problem->T; i++)
     fprintf(fp, "%02d  %3d %3d %5d\n", i, TASK.starting_date, TASK.ending_date, TASK.profit);
 #undef TASK
-  fprintf(fp,"End\n");
+  fprintf(fp, "End\n");
   //
   // terminate
   //
-  if(fflush(fp) != 0 || ferror(fp) != 0 || fclose(fp) != 0)
+  if (fflush(fp) != 0 || ferror(fp) != 0 || fclose(fp) != 0)
   {
-    fprintf(stderr,"Error while writing data to file %s\n",problem->file_name);
+    fprintf(stderr, "Error while writing data to file %s\n", problem->file_name);
     exit(1);
   }
 }
 
 #endif
 
-
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
 // main program
 //
 
-int main(int argc,char **argv)
+int main(int argc, char **argv)
 {
   problem_t problem;
-  int NMec,T,P,I;
+  int NMec, T, P, I;
 
   NMec = (argc < 2) ? 2020 : atoi(argv[1]);
   T = (argc < 3) ? 5 : atoi(argv[2]);
   P = (argc < 4) ? 2 : atoi(argv[3]);
   I = (argc < 5) ? 0 : atoi(argv[4]);
-  init_problem(NMec,T,P,I,&problem);
+  init_problem(NMec, T, P, I, &problem);
   solve(&problem);
   return 0;
 }
