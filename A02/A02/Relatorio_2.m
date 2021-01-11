bba899427ba944f4d47c4119264ed92e7c6c64c2
Pedro <pdfl@ua.pt>
2021-01-11 12:01:48 +0000

    Update Relatorio.m

--- A02/A02/Relatorio_2.m
+++ A02/A02/Relatorio_2.m
@@ -2,31 +2,33 @@
 
 sorts = ["bubble","heap","insertion","merge","quick","radix_base10","rank","selection","shaker","shell"];
 
-s = 6;                                                          % escolher posição do sort no array k se pretende avaliar 
+s = 6;                                                          % escolher posição do sort no array k se pretende avaliar
+for s = 1:6
+  a = figure(1);
+  results = load(strcat("./sorts/",sorts(s),"_sort"));
+  for k = 2:3
+      plot(logspace(0,max(results(:,1), length(results(:,k)),results(:,k));
+      hold on;
+  end
 
-results = load(strcat("./sorts/",sorts(s),"_sort"));
+  errorbar(results(:,1),results(:,4),results(:,5));               % std dev no avg time
 
-for k = 2:3
-    plot(results(:,1),results(:,k));
-    hold on;
+  hold off;
+  %ax = axes;
+  %ax.ColorOrder = [1 0 0; 0 1 0; 0 0 1; 0 0 0];
+  grid("minor");
+  xlabel("n");
+  ylabel("Execution time");
+  legend("min time","max time","avg time");
+  title(strrep(strcat(sorts(s),"_sort"),"_"," "));
+  savefig(a,strcat(sorts(s),".png"),"png");
 end
-
-errorbar(results(:,1),results(:,4),results(:,5));               % std dev no avg time
-
-hold off;
-%ax = axes;
-%ax.ColorOrder = [1 0 0; 0 1 0; 0 0 1; 0 0 0];
-grid("minor");
-xlabel("n");
-ylabel("Execution time");
-legend("min time","max time","avg time");
-title(strrep(strcat(sorts(s),"_sort"),"_"," "));
 %%
 % Comparar tds os sorts
 
 sorts = ["bubble","heap","insertion","merge","quick","radix_base10","rank","selection","shaker","shell"];
 data = ["Min time","Max time" ,"Avg Time"];
-d = 3;                                                                  % escolher o indice da data k se pretende comparar 
+d = 3;                                                                  % escolher o indice da data k se pretende comparar
 
 for s = sorts
     results = load(strcat("./sorts/",s,"_sort"));
